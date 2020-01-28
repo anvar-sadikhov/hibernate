a9667cbaf36e6972f850c22a28043d48fc070813
xD3ath <anvar.sadikhov@outlook.com>
2020-01-29 02:53:49 +0400

    new commit

--- src/main/java/com/developia/hibernate/controller/CarController.java
+++ src/main/java/com/developia/hibernate/controller/CarController.java
@@ -1,9 +1,7 @@
 package com.developia.hibernate.controller;
 
 import com.developia.hibernate.entity.Car;
-import com.developia.hibernate.repository.CarRepository;
 import com.developia.hibernate.service.CarService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
